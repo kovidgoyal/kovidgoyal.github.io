63642b3459bef73b795d449427eee547d859153c
Kovid Goyal <kovid@kovidgoyal.net>
2025-07-14 15:15:38 +0530

    Automate disk setup

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -13,6 +13,28 @@
 set -o nounset
 set -o errexit
 
+# Setup the disk (change DISK if needed on new machine)
+DISK="/dev/nvme0n1"
+sgdisk --zap-all "$DISK"
+# Create EFI System Partition: type ef00
+sgdisk --new=1:0:+550M --typecode=1:ef00 --change-name=1:"EFI System" "$DISK"
+# Create Swap Partition: type 8200
+sgdisk --new=2:0:+32G --typecode=2:8200 --change-name=2:"Linux Swap" "$DISK"
+# Create Root Partition: rest of disk, type 8300
+sgdisk --new=3:0:0 --typecode=3:8300 --change-name=3:"Linux Root" "$DISK"
+# Print the new partition table
+sgdisk -p "$DISK"
+mkfs.fat -F 32 "$DISKp1"
+mkswap "$DISKp2"
+mkfs.ext4 "$DISKp3"
+mount "$DISKp3" /mnt
+mount --mkdir "$DISKp1" /mnt/boot
+swapon "$DISKp2"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Install essential software
+pacstrap -K /mnt base linux linux-firmware iwd impala amd-ucode exfatprogs e2fsprogs nvim
+
 # Setup networking
 ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 
@@ -29,7 +51,7 @@ echo "[Security]\nPassphrase=$password" > "/mnt/var/lib/iwd/KovidTheGreat.psk"
 chmod og-rw  "/mnt/var/lib/iwd/KovidTheGreat.psk"
 cat <<EOF >> "/mnt/var/lib/iwd/KovidTheGreat.psk"
 [IPv4]
-Address=192.168.1.55
+Address=192.168.1.5
 Netmask=255.255.255.0
 Gateway=192.168.1.1
 Broadcast=192.168.1.255
@@ -44,6 +66,6 @@ chmod og-rw  "/mnt/var/lib/iwd/Westwood Villa.psk"
 
 
 # chroot into new system
-genfstab -U /mnt >> /mnt/etc/fstab
 curl 'https://raw.githubusercontent.com/kovidgoyal/kovidgoyal.github.io/refs/heads/master/setup-laptop2.sh' > /mnt/root/setup-laptop.sh
+echo "chrooting into newly installed system, run /root/setup-laptop.sh for next stage"
 arch-chroot /mnt
