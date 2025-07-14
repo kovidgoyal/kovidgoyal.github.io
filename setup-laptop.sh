5273e659f18f606be29076e8e7b8f97f9e3bc092
Kovid Goyal <kovid@kovidgoyal.net>
2025-07-14 14:30:59 +0530

    Dump systemd-networkd in favor of iwd

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -13,71 +13,37 @@
 set -o nounset
 set -o errexit
 
-# Note that currently systemd-networkd has broken matching on SSID, therefore
-# I have added a rule to the router's DHCP server to assign 192.168.1.5 by MAC address
-cat << EOF > /mnt/etc/systemd/network/20-wired.network
-[Match]
-Name=en*
-
-[Network]
-DHCP=yes
-
-[DHCPv4]
-RouteMetric=10
-
-[IPv6AcceptRA]
-RouteMetric=10
-EOF
-
-cat << EOF > /mnt/etc/systemd/network/21-wireless-home.network
-[Match]
-Name=wl*
-SSID=KovidTheGreat
-
-[Network]
-Address=192.168.1.55/24
-Gateway=192.168.1.1
-DNS=8.8.8.8
-DNS=1.1.1.1
-IgnoreCarrierLoss=3s
-EOF
-
-cat << EOF > /mnt/etc/systemd/network/30-wireless-dhcp.network
-[Match]
-Name=wl*
-
-[Network]
-DHCP=yes
-DNS=8.8.8.8
-DNS=1.1.1.1
-IgnoreCarrierLoss=3s
-
-[DHCPv4]
-RouteMetric=20
-
-[IPv6AcceptRA]
-RouteMetric=20
-EOF
-
-interface=$(ip a | grep -o ': wl.\+\?:' | cut -d' ' -f2 | cut -d: -f1)
-network_config="/mnt/etc/wpa_supplicant/wpa_supplicant-$interface.conf"
-
-mkdir -p /mnt/etc/wpa_supplicant
-cat << EOF > "$network_config"
-ctrl_interface=/run/wpa_supplicant
-ctrl_interface_group=wheel
-ap_scan=1
-country=IN
-fast_reauth=1
-
-EOF
-
+# Setup networking
 ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
-echo "Enter passphrase for KovidTheGreat"
-wpa_passphrase KovidTheGreat >> "$network_config"
-echo "Enter passphrase for Kphone"
-wpa_passphrase Kphone >> "$network_config"
-wpa_passphrase 'Westwood Villa' Westwoodvill@ >> "$network_config"
+
+mkdir -p /mnt/var/lib/iwd /mnt/etc/iwd && chmod og-rwx /mnt/var/lib/iwd
+cat <<EOF > /mnt/etc/iwd/main.conf
+[General]
+EnableNetworkConfiguration=true
+EOF
+# needed for buggy ath12k driver in current laptop (Qualcomm wifi 7)
+echo "ControlPortOverNL80211=false" >> /mnt/etc/iwd/main.conf
+echo -n "Enter passphrase for KovidTheGreat:"
+read -s password
+echo "[Security]\nPassphrase=$password" > "/mnt/var/lib/iwd/KovidTheGreat.psk"
+chmod og-rw  "/mnt/var/lib/iwd/KovidTheGreat.psk"
+cat <<EOF >> "/mnt/var/lib/iwd/KovidTheGreat.psk"
+[IPv4]
+Address=192.168.1.55
+Netmask=255.255.255.0
+Gateway=192.168.1.1
+Broadcast=192.168.1.255
+DNS=1.1.1.1
+EOF
+
+echo "[Security]\nPassphrase=$password" > "/mnt/var/lib/iwd/Kphone.psk"
+chmod og-rw  "/mnt/var/lib/iwd/Kphone.psk"
+
+echo "[Security]\nPassphrase=Westwoodvill@" > "/mnt/var/lib/iwd/Westwood Villa.psk"
+chmod og-rw  "/mnt/var/lib/iwd/Westwood Villa.psk"
+
+
+# chroot into new system
 genfstab -U /mnt >> /mnt/etc/fstab
 curl 'https://raw.githubusercontent.com/kovidgoyal/kovidgoyal.github.io/refs/heads/master/setup-laptop2.sh' > /mnt/root/setup-laptop.sh
 arch-chroot /mnt
